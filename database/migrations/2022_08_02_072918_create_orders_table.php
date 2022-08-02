7e9307d8f99bd01559c663f3f7621ad3ec63c1dd
Georges15 <georges.gerges@isae.edu.lb>
2022-08-02 11:01:52 +0300

    creating models and migrations

---
+++ database/migrations/2022_08_02_072918_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id('order_id');
+            $table->foreignId('user_id');
+            $table->foreignId('product_id');
+            $table->integer('product_quantity');
+            $table->string('payment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
